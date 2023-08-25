461fe68a1642ddabd67f3104123a46cb2001bfb8
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2023-08-25 15:02:40 +0530

    Auth and Authorization

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { error } from "console";
+import {register} from "./controllers/auth.js";
 
 /*CONFIG*/
 const __filename = fileURLToPath(import.meta.url);
@@ -21,3 +23,33 @@ app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
 app.use("/assests",express.static(path.join(__dirname,'public/assets')));
+
+/*FILE STORAGE*/
+
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,"public/assests")
+    },
+    filename: function(req,file,cb){
+        cb(null,file.originalname);
+    }
+});
+
+const upload = multer({storage});
+
+/* ROUTES WITH FILES  */
+app.post("/auth/register",upload.single("picture"),register);
+
+
+/* MONGOOSE SETUP */
+
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL,{
+
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+
+}).then(() =>{
+    app.listen(PORT,() => console.log(`Server Port:${PORT}`))
+}).catch((error) =>console.log(`${error} Didn't connected`));
+
