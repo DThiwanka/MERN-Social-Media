ea1ee55615d86819735d7aa975c7d89387afa93c
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2023-12-12 19:13:36 +0530

    Updated

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+import { Box,IconButton,InputBase,Typography,Select,MenuItem,FormControl,useTheme,useMediaQuery} from "@mui/material";
+import { Serch, Message, DarkMode, LightMode, Notification, Help, Menu, Close } from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import state, { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "components/FlexBetween";
+
 const Navbar = () => {
+
+    const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.user);
+    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+
+
+    const theme = useTheme();
+
     return <div>Navbar</div>
 }
 
