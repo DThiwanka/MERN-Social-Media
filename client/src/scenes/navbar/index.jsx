a4bd166ea367a0e23f12714472257bba2444878e
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2023-12-13 21:07:36 +0530

    Updated

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import { Box,IconButton,InputBase,Typography,Select,MenuItem,FormControl,useTheme,useMediaQuery} from "@mui/material";
-import { Serch, Message, DarkMode, LightMode, Notification, Help, Menu, Close } from "@mui/icons-material";
+import { Box, IconButton, InputBase, Typography, Select, MenuItem, FormControl, useTheme, useMediaQuery, Icon } from "@mui/material";
+import { Search, Message, DarkMode, LightMode, Notification, Help, Menu, Close } from "@mui/icons-material";
 import { useDispatch, useSelector } from "react-redux";
 import state, { setMode, setLogout } from "state";
-import { useNavigate } from "react-router-dom";
+import { Form, useNavigate } from "react-router-dom";
 import FlexBetween from "components/FlexBetween";
 
 const Navbar = () => {
@@ -16,8 +16,98 @@ const Navbar = () => {
 
 
     const theme = useTheme();
+    const neutralLight = theme.palette.neutral.light;
+    const dark = theme.palette.neutral.dark;
 
-    return <div>Navbar</div>
-}
+    const background = theme.palette.background.default;
+    const primaryLight = theme.palette.background.primaryLight;
+    const alt = theme.palette.background.alt;
+
+    const fullName = `${user.firstName} ${user.lastName}`;
+
+
+    return
+
+    <FlexBetween padding="1 rem 6%" backgroundColor={alt}>
+
+        <FlexBetween gap="1.75rem">
+            <Typography
+                fontWeight="bold"
+                fontSize="clamp(1rem,2rem,2.25rem)"
+                color="primary"
+                Onclick={() => navigate("/home")}
+                sx={{
+                    "&:hover": {
+                        color: primaryLight,
+                        cursor: "pointer",
+
+                    },
+                }}
+            >
+                Sociopedia
+            </Typography>
+            {isNonMobileScreens && (
+                <FlexBetween backgroundColor={neutralLight} borderRadius="9px" gap="3rem" padding="0.1rem 1.5rem">
+
+                    <InputBase placeholder="Search...." />
+                    <IconButton>
+                        <Search></Search>
+                    </IconButton>
+
+                </FlexBetween>
+            )}
+        </FlexBetween>
+
+        {/*Desktop*/}
+        {isNonMobileScreens ? (<FlexBetween gap="2rem">
+            <IconButton onClick={() => dispatch(setMode())}>
+                {theme.palette.mode === "dark" ? (
+                    <DarkMode sx={{fontSize : "25px"}}/>
+                ) :
+                    <LightMode sx={{ color:dark, fontSize : "25px" }}/>
+                }
+            </IconButton>
+            <Message sx={{ fontSize: "25px" }} />
+            <Notification sx={{ fontSize: "25px" }} />
+            <Help sx={{ fontSize: "25px" }} />
+            
+            <FormControl variant="standard" value={fullName}>
+                <Select value={fullName}
+                    sx={{
+                        backgroundColor: neutralLight,
+                        width: "150x",
+                        borderRadius: "0.25rem",
+                        p: "0.25rem 1rem",
+                        "& .MuiSvgIcon-root:":{
+                            pr: "0.25rem",
+                            width:"3rem"
+                        },
+                        "&.MuiSelect-select:focus": {
+                            backgroundColor:neutralLight
+                        }
+                        
+                    }}
+                    input={<InputBase />}
+                    
+                >
+                    <MenuItem value={fullName}>
+                        <Typography>
+                            {fullName}
+                        </Typography>
+                    </MenuItem>
+
+                    <MenuItem onClick={()=>dispatch(setLogout())}>Log Out</MenuItem>
+                </Select>
+            </FormControl>
+        </FlexBetween>)
+            : (<IconButton>
+                
+            </IconButton>)}
+    </FlexBetween>;
+
+};
 
 export default Navbar;
+
+
+
